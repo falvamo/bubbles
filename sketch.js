f41daf7eae5d977674c99ac0dca1404c86317340
Finley A.M.O. <fowen@student.unimelb.edu.au>
2024-09-06 16:54:32 +1000

    made the bubbles grow, shrink, and eventually disappear

--- sketch.js
+++ sketch.js
@@ -6,6 +6,10 @@ const BUBBLE_DIAMETER_MAX = 60;
 const BUBBLE_SPEED_X = 1;
 const BUBBLE_SPEED_Y = -2;
 
+// define the speed the bubbles grow and shrink
+const BUBBLE_GROWTH_SPEED = 5;
+const BUBBLE_SHRINK_SPEED = 1;
+
 // bubbles array used in the draw and mousePressed functions
 const bubbles = [];
 
@@ -21,19 +25,36 @@ class Bubble {
   constructor() {
     this.x = mouseX;
     this.y = mouseY;
-    this.d = randint(BUBBLE_DIAMETER_MIN, BUBBLE_DIAMETER_MAX);
+    this.d = 1;
+    this.isMoving = false;
   }
 
-  // draw the bubble and move it for the next frame
-  draw() {
-
-    // draw the bubble
-    circle(this.x, this.y, this.d);
-    
-    // adjust coordinates for the next frame
+  // move and shrink bubble by adjusting its coordinates and diameter
+  move() {
     this.x += BUBBLE_SPEED_X;
     this.y += BUBBLE_SPEED_Y;
+    this.d -= BUBBLE_SHRINK_SPEED;
+  }
 
+  // increment the diameter of the bubble
+  grow() {
+    this.d += BUBBLE_GROWTH_SPEED;
+  }
+
+  // draw the bubble
+  draw() {
+    circle(this.x, this.y, this.d);
+
+    // grow or move depending on the state and on user input 
+    if(!this.isMoving) {
+      if(!mouseIsPressed) {
+        this.isMoving = true;
+      } else {
+        this.grow();
+      }
+    } else {
+      this.move();
+    }
   }
 }
 
@@ -55,9 +76,20 @@ function draw() {
   fill("#FAC9B8");
   noStroke();
   
-  // draw each bubble
-  for(let bubble of bubbles) {
-    bubble.draw();
+  // draw each bubble in the array
+  for(let index in bubbles) {
+    // we use the in operator since we use the index to remove invalid bubbles
+    const bubble = bubbles[index];
+    // disallow bubbles with negative diameter
+    if(bubble.d > 0) {
+      // draw the valid bubble
+      bubble.draw();
+    } else {
+      // remove the invalid bubble from the array
+      // we note this adds a tiny 'flicker' to the game, but saves a significant amount of memory
+      // and prevents the game from taking more and more memory the more bubbles are created
+      bubbles.splice(index, 1);
+    }
   }
 
 }
